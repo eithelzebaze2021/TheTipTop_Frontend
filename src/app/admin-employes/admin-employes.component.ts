aa7643fd8a77221ff9a854ff0922255dcce83099
medyl123 <armellefopa8@gmail.com>
2023-01-26 22:26:50 +0100

    add injection service 26/1/2023

--- src/app/admin-employes/admin-employes.component.ts
+++ src/app/admin-employes/admin-employes.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClientService} from "../service/ClientService";
+import {GainService} from "../service/GainService";
+import {UserService} from "../service/UserService";
+import {EmployeService} from "../service/EmployeService";
+import {AdminService} from "../service/AdminService";
 
 @Component({
   selector: 'app-admin-employes',
   templateUrl: './admin-employes.component.html',
   styleUrls: ['./admin-employes.component.css']
 })
-export class AdminEmployesComponent {
+export class AdminEmployesComponent implements OnInit{
+
+  constructor( private clientService: ClientService,
+               private gainService: GainService,
+               private userService: UserService,
+               private employeService: EmployeService,
+               private adminService: AdminService) {
+  }
+
+  ngOnInit() {
+  }
 
 }
--- src/app/client-gain/client-gain.component.ts
+++ src/app/client-gain/client-gain.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GainService} from "../service/GainService";
+import {UserService} from "../service/UserService";
+import {ClientService} from "../service/ClientService";
 
 @Component({
   selector: 'app-client-gain',
   templateUrl: './client-gain.component.html',
   styleUrls: ['./client-gain.component.css']
 })
-export class ClientGainComponent {
+export class ClientGainComponent implements OnInit{
+
+  constructor(private gainService: GainService,
+              private userService: UserService,
+              private clientService: ClientService) {
+  }
+
+  ngOnInit(): void {
+  }
+
 
 }
