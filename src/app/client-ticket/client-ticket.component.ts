f89c9975e5b13285dd3f27b5a1cec83052443895
PAMACLA <86411862+eithelzebaze2021@users.noreply.github.com>
2023-03-15 14:59:19 +0100

    Update client-ticket.component.ts

--- src/app/client-ticket/client-ticket.component.ts
+++ src/app/client-ticket/client-ticket.component.ts
@@ -9,40 +9,79 @@ import Swal from 'sweetalert2';
 })
 export class ClientTicketComponent implements OnInit{
 
+   tabTicket: Ticket[]=[];
+
   constructor(private clientService: ClientService) {
 
   }
 
   ngOnInit(): void {
-    this.clientService.getAllTicketClient(this.clientService.clientConnect.idClient,0,15).subscribe(resp => {
+    this.clientService.getAllTicketClient(this.clientService.clientConnect.idClient).subscribe(resp => {
       if(resp.length==0){
-        Swal.fire({
-          text: "Vous n'avez pas de ticket enregistré",
-          icon:'warning',
-          showDenyButton: true,
-          confirmButtonText: "J'enregistre un ticket",
-          denyButtonText: "Pas de ticket",
-        }).then((result) => {
-          /* Read more about isConfirmed, isDenied below */
-          if (result.isConfirmed) {
-            Swal.fire({
-              title: 'Numéro ticket',
-              input: 'text',
-              inputLabel: 'Entrez le numéro du ticket',
-              inputPlaceholder: 'Entrez le numéro du ticket'
-            }).then(({ value: text })=>{
-              if (text) {
-                Swal.fire(`Le numero ticket est : ${text}`)
+         this.ajoutTicket()
+      }else{
+        this.tabTicket=resp;
+      }
+    })
+  }
 
-                this.clientService.saveTicketClient(text).subscribe(()=>{
+  ajoutTicket() {
+    Swal.fire({
+      text: "Vous n'avez pas de ticket enregistré",
+      icon:'warning',
+      showDenyButton: true,
+      confirmButtonText: "J'enregistre un ticket",
+      denyButtonText: "Pas de ticket",
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Numéro ticket',
+          input: 'text',
+          inputLabel: 'Entrez le numéro du ticket',
+          inputPlaceholder: 'Entrez le numéro du ticket'
+        }).then(({ value: text })=>{
+          if (text) {
+            Swal.fire(`Le numero ticket est : ${text}`).then((value)=>{
+              if(value){
+
+                this.clientService.saveTicketClient(text).subscribe((resp)=>{
+
+                  if(resp){
+
+                    Swal.fire({
+                      position: 'center',
+                      icon: 'success',
+                      title: 'Ticket enregistré',
+                      showConfirmButton: false,
+                      timer: 1500
+                    }).then(()=>{
+                      this.clientService.getAllTicketClient(this.clientService.clientConnect.idClient).subscribe(resp => {
+                        if(resp.length==0){
+                          this.ajoutTicket()
+                        }else{
+                          this.tabTicket=resp;
+                        }
+                      })
+                    })
+
+                  }else{
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Oops...',
+                      text: 'Numéro incorrect !'
+                    });
+                  }
 
                 })
 
               }
+
             })
 
           }
         })
+
       }
     })
   }
